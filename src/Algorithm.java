55389be64f5573dcc92b5c64b78a1d3a79bb120b
Rima-ag <rimaalghossein@gmail.com>
2021-12-17 10:30:23 +0100

    adds logic

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,25 +1,31 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Algorithm {
     public Graph bestGraph;
-    public List<List<Integer>> degree;
-    public Set<Integer> removedNodes = new HashSet<>();
+    public List<List<Integer>> nodesSortedByDegree;
+    public Integer[] degreeOfEachNode;
+    public List<List<Integer>> allEdges;
+    public Integer currentNumberOfNodes;
+    public Integer currentNumberOfEdges;
 
 
     Algorithm(String filename){
         try {
             this.bestGraph = new Graph(filename);
-            this.degree = new ArrayList<>();
+            this.nodesSortedByDegree = new ArrayList<>();
+            this.degreeOfEachNode = new Integer[bestGraph.numberOfNodes];
 
             for(int i = 0; i < bestGraph.numberOfNodes + 2; ++i)
-                degree.add(new ArrayList<>());
+                nodesSortedByDegree.add(new LinkedList<>());
 
-            for(int i = 0; i < bestGraph.neighbours.size(); ++i)
-                degree.get(bestGraph.neighbours.get(i).size()).add(i);
+            for(int i = 0; i < bestGraph.numberOfNodes; ++i) {
+                degreeOfEachNode[i] = bestGraph.neighbours.get(i).size();
+                nodesSortedByDegree.get(degreeOfEachNode[i]).add(i);
+            }
 
+            allEdges = bestGraph.neighbours;
+            currentNumberOfEdges = bestGraph.numberOfEdges;
+            currentNumberOfNodes = bestGraph.numberOfNodes;
         }catch(Exception e){
             System.out.println("Exception occurred while creating graph");
         }
@@ -27,12 +33,51 @@ public class Algorithm {
 
     public void run(){
         Integer degreeIndex = 0;
-        while(degreeIndex < degree.size()){
+        Integer node;
+        List<Integer> neighbours;
+        Integer currentNeighbour;
+        Integer currentDegree;
+        Boolean goBack;
+        while(degreeIndex < nodesSortedByDegree.size()){
             // Check if graph is complete, if yes stop else:
+            if(bestGraph.getDensity() == 2)
+                break;
             // select in node degree list and add it to set
-            // Search for neighbours
-            // -- degree of each neighbour : remove from its position in degree and put it in the one before
-            // edge cases : if no more edges left for neighbour : remove it too (i.e. if we're in index 0)
+            if(nodesSortedByDegree.get(degreeIndex).size() > 0) {
+                node = nodesSortedByDegree.get(degreeIndex).get(0);
+                nodesSortedByDegree.get(degreeIndex).remove(0);
+                --currentNumberOfNodes;
+
+                // Search for neighbours
+                neighbours = allEdges.get(node);
+                currentDegree = degreeOfEachNode[node];
+                degreeOfEachNode[node] = 0;
+                // -- degree of each neighbour : remove from its position in degree and put it in the one before
+                // edge cases : if no more edges left for neighbour : remove it too (i.e. if we're in index 0)
+                goBack = false;
+                for (int i = 0; i < neighbours.size() && !goBack; ++i) {
+                    currentNeighbour = neighbours.get(i);
+                    if (degreeOfEachNode[currentNeighbour] > 0) {
+                        nodesSortedByDegree.get(degreeOfEachNode[currentNeighbour]).remove(currentNeighbour);
+                        --degreeOfEachNode[currentNeighbour];
+                        if (degreeOfEachNode[currentNeighbour] > 0) {
+                            nodesSortedByDegree.get(degreeOfEachNode[currentNeighbour]).add(currentNeighbour);
+                            if (degreeOfEachNode[currentNeighbour] < currentDegree)
+                                goBack = true;
+                        }
+
+                        --currentNumberOfEdges;
+                    }
+                }
+                if (goBack)
+                    --degreeIndex;
+                else if (! (nodesSortedByDegree.get(degreeIndex).size() > 0))
+                    ++degreeIndex;
+            }else{
+                ++degreeIndex;
+            }
+
+
             // compare densities and update
         }
     }
